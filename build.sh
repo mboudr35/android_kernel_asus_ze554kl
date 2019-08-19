08b40e4fa7dac259927ab6e1dbe280532aee11ad
Mark Boudreau <mboudreaudalton@gmail.com>
2019-08-19 17:49:03 -0400

    build script tweaks

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 export ARCH=arm64 && export SUBARCH=arm64 && export CROSS_COMPILE=~/Projects/zenfone/gcc/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
 
-make ASUS_BUILD_PROJECT="ZE554KL" TARGET_BUILD_VARIANT="user" INSTALL_MOD_PATH=mods O=out ze554kl-user-sdm660-pastry_defconfig
-make ASUS_BUILD_PROJECT="ZE554KL" TARGET_BUILD_VARIANT="user" INSTALL_MOD_PATH=mods O=out -j$(nproc) all
+make ASUS_BUILD_PROJECT="ZE554KL" TARGET_BUILD_VARIANT="user" INSTALL_MOD_PATH=mods O=out ze554kl-user-sdm660-pastry_defconfig && \
+make ASUS_BUILD_PROJECT="ZE554KL" TARGET_BUILD_VARIANT="user" INSTALL_MOD_PATH=mods O=out -j$(nproc) all && \
 make ASUS_BUILD_PROJECT="ZE554KL" TARGET_BUILD_VARIANT="user" INSTALL_MOD_PATH=mods O=out modules_install
